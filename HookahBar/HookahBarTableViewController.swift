a7d51a3ce5c42903ab389230fe62145f0a6cb15f
Игорь Михайлович Ракитянский <i697@ya.ru>
2016-07-16 16:18:49 +0600

    UITableViewDelegate and UIAlertController

--- HookahBar/HookahBarTableViewController.swift
+++ HookahBar/HookahBarTableViewController.swift
@@ -12,6 +12,50 @@ class HookahBarTableViewController: UITableViewController {
 
     var hookahBarNames = ["Hookah Way", "Hookah Place", "The Дым", "УК", "Smoke House", "Panda", "Smoke Lab", "Hookah Time", "Fumari house", "UFS"]
     var hookahBarImages = ["hookahWay.jpg", "HP.png", "HP.png", "HP.png", "HP.png", "HP.png", "HP.png", "HP.png", "HP.png", "HP.png"]
+    var hookahBarLocation = ["Omsk", "Moskow", "Omsk", "Moskow", "Omsk", "Moskow", "Omsk", "Moskow", "Omsk", "Moskow"]
+    var hookahBarType = ["Bar", "Lounge-Bar", "Restaurant", "Bar", "Lounge-Bar", "Restaurant", "Bar", "Lounge-Bar", "Restaurant", "Bar"]
+    var hookahBarAlreadyVisited = [Bool](count: 10, repeatedValue: false)
+
+//Alert Controller
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        let actionMenu = UIAlertController(title: nil, message: "Что делаем?", preferredStyle: UIAlertControllerStyle.ActionSheet)
+
+        let cancelAction = UIAlertAction(title: "Cancel", style: UIAlertActionStyle.Cancel, handler: nil)
+        actionMenu.addAction(cancelAction)
+
+        let iHaveBeenThereAction = UIAlertAction(title: "Я тут был", style: .Default, handler: {(action: UIAlertAction) in
+            let cell = tableView.cellForRowAtIndexPath(indexPath)
+            cell?.accessoryType = UITableViewCellAccessoryType.Checkmark
+            self.hookahBarAlreadyVisited[indexPath.row] = true
+        })
+
+        let iHaveNeverBeenThereAction = UIAlertAction(title: "Я тут не был", style: .Default, handler: {(action: UIAlertAction) in
+            let cell = tableView.cellForRowAtIndexPath(indexPath)
+            cell?.accessoryType = UITableViewCellAccessoryType.None
+            self.hookahBarAlreadyVisited[indexPath.row] = false
+        })
+
+        let cell = tableView.cellForRowAtIndexPath(indexPath)
+        if cell?.accessoryType == .Checkmark {
+            actionMenu.addAction(iHaveNeverBeenThereAction)
+        } else {
+            actionMenu.addAction(iHaveBeenThereAction)
+        }
+
+        let callActionHandler = {(action: UIAlertAction!) -> Void in
+            let warningMessage = UIAlertController(title: "Сервис не доступен", message: "В данный момент вы не можите позвонить, попробуйте позже", preferredStyle: .Alert)
+            let okAction = UIAlertAction(title: "OK", style: .Default, handler: nil)
+            warningMessage.addAction(okAction)
+
+            self.presentViewController(warningMessage, animated: true, completion: nil)
+        }
+        let callAction = UIAlertAction(title: "Call Hookah Bar", style: .Destructive, handler: callActionHandler)
+        actionMenu.addAction(callAction)
+
+        self.presentViewController(actionMenu, animated: true, completion: nil)
+
+        tableView.deselectRowAtIndexPath(indexPath, animated: true)
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -47,9 +91,15 @@ class HookahBarTableViewController: UITableViewController {
 
         cell.nameLable.text = hookahBarNames[indexPath.row]
         cell.thumbnailImageView.image = UIImage(named: hookahBarImages[indexPath.row])
+        cell.lacationLable.text = hookahBarLocation[indexPath.row]
+        cell.typeLable.text = hookahBarType[indexPath.row]
 
         cell.thumbnailImageView.layer.cornerRadius = cell.thumbnailImageView.frame.size.height / 2
         cell.thumbnailImageView.clipsToBounds = true
+
+        cell.accessoryType = hookahBarAlreadyVisited[indexPath.row] ? .Checkmark : .None
+        cell.tintColor = UIColor.blackColor()
+//        cell.accessoryView = UIImageView(image: UIImage(named: "HP.png"))
 
         return cell
     }
